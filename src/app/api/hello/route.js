af3907c07c26334066dcd9c3541ec7488d98b23f
hendisantika <hendisantika@yahoo.co.id>
2024-07-10 07:36:53 +0700

    Add route.js file 🫸🌀✏️📗🐧🐳

---
+++ src/app/api/hello/route.js
@@ -0,0 +1,7 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import {pool} from "src/config/db";
+
+export default async function handler(req, res) {
+    const results = await pool.query("SELECT NOW()");
+    res.status(200).json({result: results[0]["NOW()"]});
+}
