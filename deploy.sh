e419402bec87514a698bcf92f2b0d427a4ff830a
hendisantika <hendisantika@yahoo.co.id>
2024-07-11 12:07:12 +0700

    Add deploy.sh🫸🌀✏️📗🐧🐳⬆

---
+++ deploy.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+ssh -p "${SERVER_PORT}" "${SERVER_USERNAME}"@"${SERVER_HOST}" -i key.txt -t -t -o StrictHostKeyChecking=no << 'ENDSSH'
+cd ~/product-management
+cat .env
+set +a
+source .env
+start=$(date +"%s")
+aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $AWS_ECR_REGISTRY
+docker pull $AWS_ECR_REGISTRY/$ECR_REPOSITORY:$IMAGE_TAG
+
+if [ "$(docker ps -qa -f name=$CONTAINER_NAME)" ]; then
+    if [ "$(docker ps -q -f name=$CONTAINER_NAME)" ]; then
+        echo "Container is running -> stopping it..."
+        docker system prune -af
+        docker stop $CONTAINER_NAME;
+        docker rm $CONTAINER_NAME
+    fi
+fi
+
+docker run -d --rm -p $APP_PORT:$APP_PORT --env-file .env --name $CONTAINER_NAME  $AWS_ECR_REGISTRY/$ECR_REPOSITORY:$IMAGE_TAG
+exit
+ENDSSH
+
+if [ $? -eq 0 ]; then
+  exit 0
+else
+  exit 1
+fi
+
+end=$(date +"%s")
+
+diff=$(($end - $start))
+
+echo "Deployed in : ${diff}s"
