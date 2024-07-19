b876c832e3f7986a47f5b72986029bc8a4734be5
hendisantika <hendisantika@yahoo.co.id>
2024-07-19 10:11:50 +0700

    Setup CICD :two::zero:🫸🌀✏️📗🐧🐳⬆

--- src/config/db.js
+++ src/config/db.js
@@ -2,10 +2,15 @@ import mysql from "serverless-mysql";
 
 export const pool = mysql({
     config: {
-        host: "localhost",
-        user: "root",
-        password: "S3cret",
-        port: 3307,
-        database: "productDB",
+        // host: "localhost",
+        // user: "root",
+        // password: "S3cret",
+        // port: 3307,
+        // database: "productDB",
+
+        host: process.env.MYSQL_HOST,
+        database: process.env.MYSQL_DATABASE,
+        user: process.env.MYSQL_USER,
+        password: process.env.MYSQL_PASSWORD,
     },
 });
